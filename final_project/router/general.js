a5f5c0f58f9f1160128108124634ef3526855f7d
Neiwude <awudewezah123@gmail.com>
2026-05-25 18:02:22 +0000

    fix async promise tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,7 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
   if (!username || !password) {
     return res.status(400).json({message: "Username and password are required"});
@@ -16,22 +15,32 @@ public_users.post("/register", (req,res) => {
     return res.status(409).json({message: "User already exists"});
   }
 
-  users.push({username: username, password: password});
+  users.push({username, password});
   return res.status(200).json({message: "User successfully registered. Now you can login"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
+// Get all books - async callback
+public_users.get('/', async function (req, res) {
   return res.status(200).json(books);
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN - Promise
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn]);
+
+  return new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject("Book not found");
+    }
+  })
+  .then(book => res.status(200).json(book))
+  .catch(error => res.status(404).json({message: error}));
 });
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+
+// Get book details based on author - async/await
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author.toLowerCase();
   const result = Object.values(books).filter(book =>
     book.author.toLowerCase().includes(author)
@@ -39,8 +48,8 @@ public_users.get('/author/:author',function (req, res) {
   return res.status(200).json(result);
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Get all books based on title - async/await
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title.toLowerCase();
   const result = Object.values(books).filter(book =>
     book.title.toLowerCase().includes(title)
@@ -49,57 +58,9 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 // Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   return res.status(200).json(books[isbn].reviews);
-});
-const axios = require('axios');
-
-// Get all books using async/await
-public_users.get('/asyncbooks', async function (req, res) {
-  try {
-    const response = await axios.get('http://localhost:5000/');
-    return res.status(200).json(response.data);
-  } catch (error) {
-    return res.status(500).json({message: "Error retrieving books"});
-  }
-});
-
-// Search by ISBN using Promises
-public_users.get('/promise/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-
-  axios.get(`http://localhost:5000/isbn/${isbn}`)
-    .then(response => {
-      return res.status(200).json(response.data);
-    })
-    .catch(error => {
-      return res.status(500).json({message: "Error retrieving book"});
-    });
-});
-
-// Search by Author
-public_users.get('/promise/author/:author', async function (req, res) {
-  const author = req.params.author;
-
-  try {
-    const response = await axios.get(`http://localhost:5000/author/${author}`);
-    return res.status(200).json(response.data);
-  } catch (error) {
-    return res.status(500).json({message: "Error retrieving author books"});
-  }
-});
-
-// Search by Title
-public_users.get('/promise/title/:title', async function (req, res) {
-  const title = req.params.title;
-
-  try {
-    const response = await axios.get(`http://localhost:5000/title/${title}`);
-    return res.status(200).json(response.data);
-  } catch (error) {
-    return res.status(500).json({message: "Error retrieving title books"});
-  }
 });
 
 module.exports.general = public_users;
